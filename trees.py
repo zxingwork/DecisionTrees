d5a5c94bd5823221066b090a74ff38ff55ef6705
zxingwork <zxingwork@163.com>
2020-04-21 19:16:02 +0800

    Initial commit
    Signed-off-by: zxingwork <zxingwork@163.com>

--- trees.py
+++ trees.py
@@ -3,3 +3,20 @@
 # author:zhangxing
 # datetime:2020/4/21 6:50 下午
 # software: PyCharm
+import numpy as np
+
+
+def clcShannonEnt(dataset):
+    numEntries = len(dataset)
+    labelCount = {}
+    for featVec in dataset:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCount.keys():
+            labelCount[currentLabel] = 0
+        labelCount[currentLabel] += 1
+    shannonEnt = 0.0
+    for key in labelCount:
+        prob = float(currentLabel[key]) / numEntries
+        shannonEnt -= prob * np.log2(prob)
+    return shannonEnt
+
